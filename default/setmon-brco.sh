342df6e24555b550f3b40a128f10b9ed2fe94a36
ppenguin <hieronymusv@gmail.com>
2022-10-05 15:26:12 +0200

    update setmon

--- default/setmon-brco.sh
+++ default/setmon-brco.sh
@@ -1,20 +1,71 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # set monitor brightness and contrast for all DDC capable monitors
+# TODO: mplement: from ddcutil 1.3.0 getvcp supports multiple feature codes (which may speedup execution)
 
-DDC="ddcutil --sleep-multiplier=0.1"
+DDC="ddcutil --sleep-multiplier=0.3 --brief"
 ND=$(${DDC} detect | awk '$0~/Display/ { print $2 }')
 
 B=${1:-50}
-C=${2:-50}
+C=${2:-${B}}
 
+# getincorabs <value> <increment_or_absolute>
+# does not check for incorrect format!
+getincorabs() {
+    case ${2} in
+        -*|+*)
+            echo "$((${1}${2}))"
+            ;;
+        *)
+            echo "${2}"
+            ;;
+    esac
+}
+
+# getvcp <displaynr> <feature> returns int value of the feature
+getvcp() {
+    ${DDC} -d "${1}" getvcp "${2}" | awk '{ print $4 }'
+}
+
+# setvcp <displaynr> <feature> <tgtvalue> returns <tgtvalue>
+setvcp() {
+    ${DDC} -d "${1}" setvcp "${2}" "${3}" 1>&2
+    echo "${3}"
+}
+
+# getsetvcp <displaynr> <feature> <tgtvalue> returns old and new value
+getsetvcp() {
+    curval=$(getvcp "${1}" "${2}")
+    tgtval=$(getincorabs "${curval}" "${3}")
+    res=$(setvcp "${1}" "${2}" "${tgtval}")
+    echo "${curval} ${res}"
+}
+
+# setbrightness <displaynr> <incr_or_abs>
+setbrightness() {
+    printf "display %s brightness %s -> %s\n" ${1} $(getsetvcp ${1} 10 ${2}) >&2
+}
+
+# setbrightness <displaynr> <incr_or_abs>
+setcontrast() {
+    printf "display %s contrast %s -> %s\n" ${1} $(getsetvcp ${1} 12 ${2}) >&2
+}
+
+# setbrco <displaynr> <br> [<co>]
+# set both brightness and contrast with the same value if contrast is not given
+setbrco() {
+    disp=${1}
+    br=${2}
+    if [ -n "${3}" ]; then
+        co=${3}
+    else
+        co=${2}
+    fi;
+    setbrightness ${disp} ${br}
+    setcontrast ${disp} ${co}
+}
+
+# different monitors async (with &) didn't work...
 for N in ${ND}; do
-    >&2 printf "Setting display %d:\n\tbrightness %d => %d\n\tcontrast %d => %d\n" \
-        "${N}" \
-        "$(${DDC} -d ${N} getvcp 10 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
-        "${B}" \
-        "$(${DDC} -d ${N} getvcp 12 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
-        "${C}"
-    ${DDC} -d ${N} setvcp 10 ${B}
-    ${DDC} -d ${N} setvcp 12 ${C}
+    setbrco ${N} ${B} ${C}
 done
