6cd14e230703894b013b09105276b483802d833d
ppenguin <hieronymusv@gmail.com>
2022-10-05 14:08:14 +0200

    speedup setmon (?)

--- default/setmon-brco.sh
+++ default/setmon-brco.sh
@@ -2,7 +2,8 @@
 
 # set monitor brightness and contrast for all DDC capable monitors
 
-ND=$(ddcutil detect | awk '$0~/Display/ { print $2 }')
+DDC="ddcutil --sleep-multiplier=0.1"
+ND=$(${DDC} detect | awk '$0~/Display/ { print $2 }')
 
 B=${1:-50}
 C=${2:-50}
@@ -10,10 +11,10 @@ C=${2:-50}
 for N in ${ND}; do
     >&2 printf "Setting display %d:\n\tbrightness %d => %d\n\tcontrast %d => %d\n" \
         "${N}" \
-        "$(ddcutil -d ${N} getvcp 10 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
+        "$(${DDC} -d ${N} getvcp 10 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
         "${B}" \
-        "$(ddcutil -d ${N} getvcp 12 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
+        "$(${DDC} -d ${N} getvcp 12 | sed -nr 's/^.*current value =\s*([0-9]*).*$/\1/p')" \
         "${C}"
-    ddcutil -d ${N} setvcp 10 ${B}
-    ddcutil -d ${N} setvcp 12 ${C}
+    ${DDC} -d ${N} setvcp 10 ${B}
+    ${DDC} -d ${N} setvcp 12 ${C}
 done
